08acd7b17eb97dcc72909c05d6735ae8ada17551
Daniel Leong <me@dhleong.net>
2018-06-08 18:33:25 -0400

    Parse all (I think) creature stats. Next step, text stuff!

--- src/parser/creature-part.ts
+++ src/parser/creature-part.ts
@@ -1,6 +1,62 @@
 import { ICreaturePart, IStringPart, PartType } from './interface';
 import { Section } from './section';
 
+export enum Alignment {
+    LawfulGood,
+    LawfulNeutral,
+    LawfulEvil,
+
+    NeutralGood,
+    TrueNeutral,
+    NeutralEvil,
+
+    ChaoticGood,
+    ChaoticNeutral,
+    ChaoticEvil,
+}
+
+export function alignmentFromString(alignmentStr: string): Alignment {
+    if (alignmentStr === 'neutral') {
+        return Alignment.TrueNeutral;
+    } else {
+        const [lawVsChaos, goodVsEvil] = alignmentStr
+            .toLowerCase().split(' ');
+        let align = 0;
+
+        if (lawVsChaos[0] === 'n') {
+            align += Alignment.NeutralGood;
+        } else if (lawVsChaos[0] === 'c') {
+            align += Alignment.ChaoticGood;
+        }
+
+        if (goodVsEvil[0] === 'n') {
+            align += 1;
+        } else if (goodVsEvil[0] === 'e') {
+            align += 2;
+        }
+
+        return align;
+    }
+}
+
+export enum Size {
+    Tiny,
+    Small,
+    Medium,
+    Large,
+    Huge,
+    Gargantuan,
+}
+
+const sizeByString = {
+    gargantuan: Size.Gargantuan,
+    huge: Size.Huge,
+    large: Size.Large,
+    medium: Size.Medium,
+    small: Size.Small,
+    tiny: Size.Tiny,
+};
+
 class Abilities {
     constructor(
         readonly str: number,
@@ -8,6 +64,7 @@ class Abilities {
         readonly con: number,
         readonly int: number,
         readonly wis: number,
+        readonly cha: number,
     ) {}
 }
 
@@ -35,18 +92,44 @@ export class CreaturePart implements ICreaturePart {
         [creature.hp, creature.hpRoll] = splitByNumber(firstMap['Hit Points']);
         creature.speed = firstMap.Speed;
 
+        const [sizeAndKind, alignmentStr] = firstMap[0].split(', ');
+        const [sizeRaw, kind] = sizeAndKind.split(' ');
+        creature.size = sizeByString[sizeRaw.toLowerCase()];
+        creature.kind = kind;
+        creature.align = alignmentFromString(alignmentStr);
+
         const nextMap = parts.length >= 3
             ? (parts[2] as IStringPart).toMapBySpans()
             : null;
 
         if (nextMap) {
             const rawAbilities: string = nextMap[0];
-            const [str, dex, con, int, wis] =
+            const [str, dex, con, int, wis, cha] =
                 rawAbilities.split(/\([-−\+0-9]+\)/)
                 .map(raw => parseInt(raw.trim(), 10));
             creature.abilities = new Abilities(
-                str, dex, con, int, wis,
+                str, dex, con, int, wis, cha,
             );
+
+            // TODO this could be an Abilities, perhaps?
+            creature.savingThrows = nextMap['Saving Throws'];
+
+            // TODO this could be a separate type,
+            // eg: {passivePerception, darkvision, truesight, etc}
+            creature.senses = nextMap.Senses;
+
+            // TODO this could also be a separate type
+            creature.skills = nextMap.Skills;
+
+            creature.immunities = nextMap['Damage Immunities'];
+            creature.resistances = nextMap['Damage Resistances'];
+            creature.vulnerabilities = nextMap['Damage Vulnerabilities'];
+            creature.conditionImmunities = nextMap['Condition Immunities'];
+
+            creature.languages = nextMap.Languages;
+            const [cr, rawExp] = splitByNumber(nextMap.Challenge);
+            creature.cr = cr;
+            creature.exp = parseInt(rawExp.replace(/[, ()]/g, ''), 10);
         }
 
         // TODO
@@ -56,6 +139,9 @@ export class CreaturePart implements ICreaturePart {
     type = PartType.CREATURE;
 
     name: string;
+    size: Size;
+    kind: string;
+    align: Alignment;
 
     ac: number;
     acSource: string;
@@ -65,6 +151,18 @@ export class CreaturePart implements ICreaturePart {
     speed: string;
 
     abilities: Abilities;
+
+    savingThrows: string;
+    senses: string;
+    skills: string;
+    immunities: string;
+    resistances: string;
+    vulnerabilities: string;
+    conditionImmunities: string;
+
+    languages: string;
+    cr: number;
+    exp: number;
 
     postProcess() {
         /* nop */
--- test/parser/creature-part-test.ts
+++ test/parser/creature-part-test.ts
@@ -2,9 +2,29 @@ import * as chai from 'chai';
 
 import { loadJsonSections } from '../test-utils';
 
-import { CreaturePart } from '../../src/parser/creature-part';
+import {
+    Alignment, alignmentFromString,
+    CreaturePart,
+    Size,
+} from '../../src/parser/creature-part';
 
 chai.should();
+
+describe('alignmentFromString', () => {
+    it('works', () => {
+        alignmentFromString('lawful good').should.equal(Alignment.LawfulGood);
+        alignmentFromString('lawful neutral').should.equal(Alignment.LawfulNeutral);
+        alignmentFromString('lawful evil').should.equal(Alignment.LawfulEvil);
+
+        alignmentFromString('neutral good').should.equal(Alignment.NeutralGood);
+        alignmentFromString('neutral').should.equal(Alignment.TrueNeutral);
+        alignmentFromString('neutral evil').should.equal(Alignment.NeutralEvil);
+
+        alignmentFromString('chaotic good').should.equal(Alignment.ChaoticGood);
+        alignmentFromString('chaotic neutral').should.equal(Alignment.ChaoticNeutral);
+        alignmentFromString('chaotic evil').should.equal(Alignment.ChaoticEvil);
+    });
+});
 
 describe('CreaturePart', () => {
     describe('parsing', () => {
@@ -16,12 +36,32 @@ describe('CreaturePart', () => {
             const part = await creaturePromise;
 
             part.name.should.equal('Aboleth');
+            part.size.should.equal(Size.Large);
+            part.kind.should.equal('aberration');
+            part.align.should.equal(Alignment.LawfulEvil);
+
             part.ac.should.equal(17);
             part.acSource.should.equal('(natural armor)');
             part.hp.should.equal(135);
             part.hpRoll.should.equal('(18d10 + 36)');
+            part.speed.should.equal('10 ft., swim 40 ft.');
 
             part.abilities.str.should.equal(21);
+            part.abilities.dex.should.equal(9);
+            part.abilities.con.should.equal(15);
+            part.abilities.int.should.equal(18);
+            part.abilities.wis.should.equal(15);
+            part.abilities.cha.should.equal(18);
+
+            part.savingThrows.should.equal('Con +6, Int +8, Wis +6');
+            part.senses.should.equal('darkvision 120 ft., passive Perception 20');
+            part.skills.should.equal('History +12, Perception +10');
+            part.should.have.property('immunities').that.is.undefined;
+            part.should.have.property('resistances').that.is.undefined;
+
+            part.languages.should.equal('Deep Speech, telepathy 120 ft.');
+            part.cr.should.equal(10);
+            part.exp.should.equal(5900);
         });
     });
 });
